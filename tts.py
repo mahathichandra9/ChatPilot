7322ec5cae3fcaeab05a0d8c3cf104483c0f9797
Sairam <sairammekala7@gmail.com>
2025-09-27 23:36:42 +0530

    Paper on vision based navigation

--- tts.py
+++ tts.py
@@ -5,8 +5,6 @@ engine = pyttsx3.init()
 engine.setProperty("volume", 1)
 engine.setProperty("rate", 130)
 voices = engine.getProperty("voices")
-# for i in voices:
-#     print(i)
 engine.setProperty("voice", voices[1].id)
 engine.say("Hello sir, How can I help you?")
 engine.runAndWait()
