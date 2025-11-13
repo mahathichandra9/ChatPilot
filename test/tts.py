ed5b978069f494c1e33a84e981bb87007df69c76
Sairam <sairammekala7@gmail.com>
2025-11-14 00:03:35 +0530

    Organised repo

--- test/tts.py
+++ test/tts.py
@@ -2,9 +2,9 @@ import pyttsx3
 engine = pyttsx3.init() # object creation
 # VOICE
 voices = engine.getProperty('voices')       # getting details of current voice
-engine.setProperty('voice', voices[0].id)  # changing index, changes voices. o for male
+engine.setProperty('voice', voices[1].id)  # changing index, changes voices. o for male
 # engine.setProperty('voice', voices[1].id)  
-engine.say("Hello World!")
+engine.say("Hello sherlock sampath!")
 # engine.say('My current speaking rate is ' + str(rate))
 engine.runAndWait()
 engine.stop()
